322792ac77dfc9af5940e117f480961d4cbeee60
OllieSAE <1020925@student.sae.edu.au>
2023-05-31 17:11:33 +1000

    movement cont
    jump works, but can multi jump
    trying to fix dash but FixedUpdate is preventing it atm

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -10,9 +10,14 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rigidbody;
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
+    private Vector2 inputVector;
 
+    //delete once L/R sprites exist
+    private bool facingLeft;
+    
     public float speed;
     public float jumpValue;
+    public float dashValue;
 
     private void Awake()
     {
@@ -22,17 +27,28 @@ public class Movement : MonoBehaviour
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Jump.performed += Jump;
+        playerInputActions.Player.Jump.canceled += ArrestJump;
         playerInputActions.Player.Movement.performed += Move;
         playerInputActions.Player.Dash.performed += Dash;
         playerInputActions.Player.Crouch.performed += Crouch;
     }
 
+    private void Update()
+    {
+        inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+    }
+
     private void FixedUpdate()
     {
-        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        rigidbody.AddForce(new Vector3(inputVector.x,0,inputVector.y) * speed, ForceMode2D.Force);
-
-        //maybe cap speed at 5 and apply 1000 force?
+        rigidbody.velocity += new Vector2(inputVector.x * speed * Time.fixedDeltaTime, rigidbody.velocity.y);
+        if (inputVector.x > 0 && facingLeft)
+        {
+            Flip();
+        }
+        else if (inputVector.x < 0 && !facingLeft)
+        {
+            Flip();
+        }
     }
 
 
@@ -49,13 +65,41 @@ public class Movement : MonoBehaviour
         
     }
 
+    public void ArrestJump(InputAction.CallbackContext context)
+    {
+        if(rigidbody.velocity.y > 0) rigidbody.velocity = new Vector2(rigidbody.velocity.x,0);
+    }
+
     public void Dash(InputAction.CallbackContext context)
     {
-        if(context.performed) ;
+        if (context.performed)
+        {
+            if (!facingLeft)
+            {
+                rigidbody.AddForce(Vector3.right * dashValue, ForceMode2D.Impulse);
+                print("dash right" + rigidbody.velocity.x);
+            }
+
+            if (facingLeft)
+            {
+                rigidbody.AddForce(Vector3.left * dashValue, ForceMode2D.Impulse);
+                print("dash left" + rigidbody.velocity.x);
+            }
+        }
     }
 
     public void Crouch(InputAction.CallbackContext context)
     {
         if(context.performed) ;
+    }
+
+    //Temporary fix for placeholder sprites
+    //will replace with actual L/R sprites
+    public void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
