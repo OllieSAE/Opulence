d364a49c67ef2cc8b9466af9589ada0795061f10
OllieSAE <1020925@student.sae.edu.au>
2023-05-31 12:06:30 +1000

    player action map + movement framework set up

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -1,18 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody rigidbody;
+    private PlayerInput playerInput;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        playerInputActions.Player.Movement.performed += Move;
+        playerInputActions.Player.Dash.performed += Dash;
+        playerInputActions.Player.Crouch.performed += Crouch;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Jump(InputAction.CallbackContext context)
     {
-        
+        if(context.performed) ;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if(context.performed) ;
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if(context.performed) ;
+    }
+
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        if(context.performed) ;
     }
 }
