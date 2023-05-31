171f9a0ee77d3ec2c3f7f860486fd39f6ffb667e
OllieSAE <1020925@student.sae.edu.au>
2023-05-31 15:07:25 +1000

    movement still WIP

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -7,15 +7,19 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
 
-    private Rigidbody rigidbody;
+    private Rigidbody2D rigidbody;
     private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    public float speed;
+    public float jumpValue;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
 
-        PlayerInputActions playerInputActions = new PlayerInputActions();
+        playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Jump.performed += Jump;
         playerInputActions.Player.Movement.performed += Move;
@@ -23,15 +27,26 @@ public class Movement : MonoBehaviour
         playerInputActions.Player.Crouch.performed += Crouch;
     }
 
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        rigidbody.AddForce(new Vector3(inputVector.x,0,inputVector.y) * speed, ForceMode2D.Force);
+
+        //maybe cap speed at 5 and apply 1000 force?
+    }
+
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.performed) ;
+        if (context.performed)
+        {
+            rigidbody.AddForce(Vector3.up * jumpValue, ForceMode2D.Impulse);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        if(context.performed) ;
+        
     }
 
     public void Dash(InputAction.CallbackContext context)
