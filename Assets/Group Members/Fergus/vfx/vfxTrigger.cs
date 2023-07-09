2abc8c888bcbc3cab05222c244ac1ca34a28fff4
fergus-hamilton <1022312@student.sae.edu.au>
2023-07-09 20:09:19 +1000

    added a munch more VFX
    more more more, vfx

--- Assets/Group Members/Fergus/vfx/vfxTrigger.cs
+++ Assets/Group Members/Fergus/vfx/vfxTrigger.cs
@@ -5,28 +5,47 @@ using UnityEngine.VFX;
 
 public class vfxTrigger : MonoBehaviour
 {
-    public VisualEffect vfx;
+    public VisualEffect[] vfx;
+    public ParticleSystem[] particles;
+    public Transform flipTransform;
+    private bool facingLeft = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        vfx = GetComponentInChildren<VisualEffect>();
-        vfx.enabled = false;
+        if (vfx == null) vfx[0] = GetComponentInChildren<VisualEffect>();
+        if (vfx[0].enabled) vfx[0].enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        for (int i = 0; i < particles.Length; i++)
+        {
+            particles[i].transform.localScale = flipTransform.localScale; 
+        }
         
     }
 
     public IEnumerator VFXTrigger()
     {
-        if (!vfx.enabled)
+
+        for (int i = 0; i < vfx.Length; i++)
         {
-            vfx.enabled = true;
-            yield return new WaitForSeconds(0.3f);
-            vfx.enabled = false;
+            if (!vfx[i].enabled)
+            {
+                vfx[i].enabled = true;
+                yield return new WaitForSeconds(0.3f);
+                vfx[i].enabled = false;
+            }
+        }
+    }
+
+    public void ParticleTrigger()
+    {
+        for (int i = 0; i < particles.Length; i++)
+        {
+            particles[i].Play();
         }
     }
 }
