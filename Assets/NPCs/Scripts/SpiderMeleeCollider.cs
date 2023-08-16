b04ae2b0a170f7aeb921fe989fc7535cb8e12f59
OllieSAE <1020925@student.sae.edu.au>
2023-08-16 13:01:09 +1000

    Revert "boss finished!"
    This reverts commit 48e40445a4dcd7d723ed69851942a5d2fd2d137c.

--- Assets/NPCs/Scripts/SpiderMeleeCollider.cs
+++ Assets/NPCs/Scripts/SpiderMeleeCollider.cs
@@ -1,45 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Cinemachine;
 using UnityEngine;
 
 public class SpiderMeleeCollider : MonoBehaviour
 {
     private int meleeAttackPower;
-    public Combat combat;
-    public Vector2 overlapBoxSize;
-    public Vector2 overlapBoxPos;
-    public Vector2 newPos;
-    private bool damageDealt = false;
+    private Combat combat;
+    public BoxCollider2D collider;
     
     public int damageModifier = 1;
 
+    private void Start()
+    {
+        combat = GetComponentInParent<Combat>();
+    }
+
     private void OnEnable()
     {
-        meleeAttackPower = combat.meleeAttackPower;
-        damageDealt = false;
+        collider.enabled = true;
     }
 
-    private void Update()
+    //this DOES NOT WORK if the player is stationary
+    //because unity is doing a bit... again
+    //maybe attempt a physics2D circle check, like the original charger?
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        newPos = overlapBoxPos;
-        newPos.x += transform.position.x;
-        newPos.y += transform.position.y;
-        Collider2D[] hitPlayers = Physics2D.OverlapBoxAll(newPos, overlapBoxSize, 0.5f);
-        foreach (Collider2D hitPlayer in hitPlayers)
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (hitPlayer.CompareTag("Player") && !damageDealt)
-            {
-                damageDealt = true;
-                hitPlayer.GetComponent<Health>().ChangeHealth(-meleeAttackPower*damageModifier,this.gameObject);
-            }
+            meleeAttackPower = combat.meleeAttackPower;
+            other.GetComponentInParent<Health>().ChangeHealth(-meleeAttackPower * damageModifier,this.gameObject);
+            gameObject.SetActive(false);
         }
     }
-
-    // private void OnDrawGizmos()
-    // {
-    //     Gizmos.color = Color.red;
-    //     Gizmos.DrawCube(newPos,overlapBoxSize);
-    // }
 }
